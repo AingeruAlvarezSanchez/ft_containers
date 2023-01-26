6fc25446dc5b247a135755a8edb56bc7adec68ff
AingeruAlvarezSanchez <aalvarez@student.42urduliz.com>
2023-01-26 20:45:50 +0100

    equal and lexicographical compare

--- lexicographical_compare.hpp
+++ lexicographical_compare.hpp
@@ -1,4 +1,33 @@
 #ifndef LEXICOGRAPHICAL_COMPARE_HPP
 #define LEXICOGRAPHICAL_COMPARE_HPP
 
+namespace ft {
+	template <class InputIterator1, class InputIterator2>
+	bool	lexicographical_compare(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2) {
+		while (first1 != last1) {
+			if ((first2 == last2) || *first2 < *first1)
+				return false;
+			else if (*first1 < *first2)
+				return true;
+			++first1;
+			++first2;
+		}
+		return first2 != last2;
+	}
+
+	template <class InputIterator1, class InputIterator2, class Compare>
+	bool	lexicographical_compare(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, Compare comp) {
+		while (first1 != last1) {
+			if ((first2 == last2) || comp(*first2, *first1))
+				return false;
+			else if (comp(*first1, *first2))
+				return true;
+			++first1;
+			++first2;
+		}
+		return first2 != last2;
+	}
+	//TODO check for both those functions, they may not work as expected due to my implementation
+}
+
 #endif //LEXICOGRAPHICAL_COMPARE_HPP
